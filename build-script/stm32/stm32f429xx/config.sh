567f6c35314ae404bd6bdd7cf040176a698fc94b
zhenquan.qiu <gnsyxiang@163.com>
2025-12-23 16:15:42 +0800

    build: update build script

--- build-script/stm32/stm32f429xx/config.sh
+++ build-script/stm32/stm32f429xx/config.sh
@@ -9,11 +9,11 @@ product=stm32
 language=cn en
 
 host=arm-none-eabi
-cross_gcc_path=/opt/toolchains/mcu/gcc-arm-none-eabi-6_2-2016q4/bin/arm-none-eabi-
+cross_gcc_path=/opt/toolchains/mcu/arm-gnu-toolchain-11.3.rel1-x86_64-arm-none-eabi/bin/arm-none-eabi-
 
 configure_param=
 
-cppflag=-mcpu=cortex-m4 -mthumb -mfpu=fpv4-sp-d16 -mfloat-abi=hard -ffunction-sections -fdata-sections -I/opt/toolchains/mcu/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include
+cppflag=-mcpu=cortex-m4 -mthumb -mfpu=fpv4-sp-d16 -mfloat-abi=hard -ffunction-sections -fdata-sections -I/opt/toolchains/mcu/arm-gnu-toolchain-11.3.rel1-x86_64-arm-none-eabi/include
 cflag=-Dgcc -std=c99 -DUSE_HAL_DRIVER -DSTM32F429xx
 cxxflag=
 ldflag=-mcpu=cortex-m4 -mthumb -mfpu=fpv4-sp-d16 -mfloat-abi=hard -specs=nano.specs -specs=nosys.specs
