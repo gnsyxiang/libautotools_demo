7a4e87967e0e74c92f629c369bceec67f379146f
zhenquan.qiu <gnsyxiang@163.com>
2026-09-04 09:48:35 +0800

    build: update gcc for mcu

--- build-script/stm32/stm32f429xx/config.sh
+++ build-script/stm32/stm32f429xx/config.sh
@@ -2,23 +2,23 @@
 
 chip=stm32f429xx
 
-os=freertos
+os=freertos mcu
 
 product=stm32
 
 language=cn en
 
 host=arm-none-eabi
-cross_gcc_path=/opt/toolchains/mcu/arm-gnu-toolchain-11.3.rel1-x86_64-arm-none-eabi/bin/arm-none-eabi-
+cross_gcc_path=/opt/toolchains/mcu/arm-gnu-toolchain_arm-none-eabi/bin/arm-none-eabi-
 
 configure_param=
 
-cppflag=-mcpu=cortex-m4 -mthumb -mfpu=fpv4-sp-d16 -mfloat-abi=hard -ffunction-sections -fdata-sections -I/opt/toolchains/mcu/arm-gnu-toolchain-11.3.rel1-x86_64-arm-none-eabi/include
+cppflag=-mcpu=cortex-m4 -mthumb -mfpu=fpv4-sp-d16 -mfloat-abi=hard -I/opt/toolchains/mcu/arm-gnu-toolchain_arm-none-eabi/arm-none-eabi/include
 cflag=-Dgcc -std=c99 -DUSE_HAL_DRIVER -DSTM32F429xx
 cxxflag=
 ldflag=-mcpu=cortex-m4 -mthumb -mfpu=fpv4-sp-d16 -mfloat-abi=hard -specs=nano.specs -specs=nosys.specs
 lib=-lc -lm -lnosys
-debug= -O0 -gdwarf-2 -g
+debug= -Og -g -gdwarf-2
 release=-Os
 
 install_path=/mnt/nfs/stm32/stm32f429xx
--- build-script/stm32/stm32h743xx/config.sh
+++ build-script/stm32/stm32h743xx/config.sh
@@ -9,16 +9,16 @@ product=stm32
 language=cn en
 
 host=arm-none-eabi
-cross_gcc_path=/opt/toolchains/mcu/arm-gnu-toolchain-11.3.rel1-x86_64-arm-none-eabi/bin/arm-none-eabi-
+cross_gcc_path=/opt/toolchains/mcu/arm-gnu-toolchain_arm-none-eabi/bin/arm-none-eabi-
 
 configure_param=
 
-cppflag=-mcpu=cortex-m7 -mthumb -mfpu=fpv5-d16 -mfloat-abi=hard -ffunction-sections -fdata-sections -I/opt/toolchains/mcu/arm-gnu-toolchain-11.3.rel1-x86_64-arm-none-eabi/include
+cppflag=-mcpu=cortex-m7 -mthumb -mfpu=fpv5-d16 -mfloat-abi=hard -I/opt/toolchains/mcu/arm-gnu-toolchain_arm-none-eabi/arm-none-eabi/include
 cflag=-Dgcc -std=c99 -DUSE_HAL_DRIVER -DSTM32H743xx
 cxxflag=
 ldflag=-mcpu=cortex-m7 -mthumb -mfpu=fpv5-d16 -mfloat-abi=hard -specs=nano.specs -specs=nosys.specs
 lib=-lc -lm -lnosys
-debug= -O0 -gdwarf-2 -g
+debug= -Og -g -gdwarf-2
 release=-Os
 
 install_path=/mnt/nfs/stm32/stm32h743xx
